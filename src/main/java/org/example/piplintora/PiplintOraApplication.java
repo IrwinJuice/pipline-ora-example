940d739e30fcea5a0fa9b1f2e5a88ae848551c62
IrwinJuice <sevriukovmk@gmail.com>
2024-02-25 12:02:14 +0200

    first commit

---
+++ src/main/java/org/example/piplintora/PiplintOraApplication.java
@@ -0,0 +1,13 @@
+package org.example.piplintora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiplintOraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiplintOraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/piplintora/PiplintOraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.piplintora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiplintOraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
